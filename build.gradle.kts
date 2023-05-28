df03264c2359ec3e7dc750d8a50e397bf1ad42c4
mvukic <matija.vukic2@gmail.com>
2023-05-28 14:30:34 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.9.0-Beta"
+    kotlin("plugin.serialization") version "1.8.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(20)
+}
+
+tasks {
+    wrapper {
+        version = "8.1.1"
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    println("main")
+    for (n in 2..<10) {
+        println(n)
+    }
+}
