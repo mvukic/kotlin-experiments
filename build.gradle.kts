e7148b2dbaf0465e35d1d12310b0f1650f348f1a
mvukic <matija.vukic2@gmail.com>
2023-09-10 12:19:50 +0200

    updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("org.springframework.boot") version "3.2.0-M2"
     id("io.spring.dependency-management") version "1.1.3"
@@ -8,25 +10,28 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
+kotlin {
+    jvmToolchain(20)
+}
+
 repositories {
     mavenCentral()
     maven { url = uri("https://repo.spring.io/milestone") }
 }
 
+ext["kotlin-coroutines.version"] = "1.7.3"
+
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     implementation(kotlinx("datetime:0.4.0"))
     implementation(kotlinx("serialization-json:1.5.1"))
-    implementation(kotlinx("coroutines-core:1.7.3"))
+    implementation(kotlinx("coroutines-core"))
+    implementation(kotlinx("coroutines-reactor"))
 
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation(kotlin("reflect"))
-    implementation(kotlinx("coroutines-reactor"))
-}
-
-kotlin {
-    jvmToolchain(20)
 }
 
 tasks {
@@ -36,6 +41,13 @@ tasks {
 
     wrapper {
         version = "8.3"
+    }
+
+    compileKotlin {
+        compilerOptions {
+            freeCompilerArgs.set(listOf("-Xjsr305=strict"))
+            jvmTarget.set(JvmTarget.JVM_20)
+        }
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,7 @@
 pluginManagement {
     repositories {
         maven { url = uri("https://repo.spring.io/milestone") }
-        mavenCentral()
         gradlePluginPortal()
     }
-
 }
 rootProject.name = "kotlin-experiments"
-
