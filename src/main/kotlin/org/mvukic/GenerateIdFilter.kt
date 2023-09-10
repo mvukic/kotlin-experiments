a5d3582924e00221fba5984d21e052f71d1b5309
mvukic <matija.vukic2@gmail.com>
2023-09-10 14:19:44 +0200

    updates

--- src/main/kotlin/org/mvukic/GenerateIdFilter.kt
+++ src/main/kotlin/org/mvukic/GenerateIdFilter.kt
@@ -1,6 +1,6 @@
 package org.mvukic
 
-import io.klogging.Klogging
+import io.klogging.java.LoggerFactory
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
@@ -9,33 +9,71 @@ import org.springframework.web.server.CoWebFilterChain
 import org.springframework.web.server.ServerWebExchange
 import java.util.*
 
+
+//@Configuration
+//@Order(Ordered.HIGHEST_PRECEDENCE)
+//class ErrorHandler : ErrorWebExceptionHandler {
+//    override fun handle(exchange: ServerWebExchange, ex: Throwable): Mono<Void> {
+//        return Mono.empty()
+//    }
+//}
+
+data class User(val name: String)
+
+
+// Generate id
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-class RequestStartFilter : CoWebFilter(), Klogging {
+class RequestStartFilter : CoWebFilter() {
+
+    private val logger = LoggerFactory.getLogger(RequestStartFilter::class.java)
+
     override suspend fun filter(exchange: ServerWebExchange, chain: CoWebFilterChain) {
         val request = exchange.request
 
         val requestId = UUID.randomUUID().toString()
+
+        // Save id as an exchange attribute
+        exchange.attributes["requestId"] = requestId
+
         val method = request.method.toString()
         val path = request.path.value()
-
         logger.info("RequestStartFilter {requestId}, {method}, {path}", requestId, method, path)
 
-        val requestCopy = request.mutate().header("requestId", requestId).build()
-        val exchangeCopy = exchange.mutate().request(requestCopy).build()
-        return chain.filter(exchangeCopy)
+        return chain.filter(exchange)
     }
 }
 
+// Get and save user data
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE + 1)
+class AuthenticationFilter : CoWebFilter() {
+
+    private val logger = LoggerFactory.getLogger(AuthenticationFilter::class.java)
+
+    override suspend fun filter(exchange: ServerWebExchange, chain: CoWebFilterChain) {
+        val user = /* get user from request */ User("user")
+
+        // Save user as an exchange attribute
+        exchange.attributes["user"] = user
+
+        logger.info("AuthenticationFilter {user}", user.name)
+
+        return chain.filter(exchange)
+    }
+}
+
+
+// Log end request
 @Component
 @Order(Ordered.LOWEST_PRECEDENCE)
-class RequestEndFilter : CoWebFilter(), Klogging {
+class RequestEndFilter : CoWebFilter() {
+
+    private val logger = LoggerFactory.getLogger(RequestEndFilter::class.java)
+
     override suspend fun filter(exchange: ServerWebExchange, chain: CoWebFilterChain) {
-        val request = exchange.request
-        val requestId = request.headers["requestId"]
-
-        logger.info("RequestEndFilter {requestId}", requestId)
-
+        val requestId = exchange.getAttribute<String>("requestId")!!
+        logger.info("RequestEndFilter {requestId} {user}", requestId)
         return chain.filter(exchange)
     }
 }
--- src/main/kotlin/org/mvukic/main.kt
+++ src/main/kotlin/org/mvukic/main.kt
@@ -1,6 +1,9 @@
 package org.mvukic
 
 import io.klogging.Klogging
+import io.klogging.context.Context
+import io.klogging.context.withLogContext
+import kotlinx.coroutines.reactor.ReactorContext
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -10,6 +13,7 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.buildAndAwait
 import org.springframework.web.reactive.function.server.coRouter
+import kotlin.coroutines.coroutineContext
 
 
 @Service
@@ -33,5 +37,20 @@ class RouterClass(private val handler: RouterHandler) {
 class SpringBootApp
 
 fun main(args: Array<String>) {
+    // Always logged
+    Context.addBaseContext(
+        "app" to "LoggingDemo",
+        "version" to "1.2.0"
+    )
+
+    // Always logged but dynamically fetched from the ReactorContext
+    Context.addContextItemExtractor(ReactorContext) { ctx ->
+        // Get request id
+        val requestId = ctx.context.getOrDefault<String>("requestId", null) ?: "Not found"
+        // Get user (optional)
+        val user = ctx.context.getOrDefault<User>("user", null)
+        mapOf("requestId" to requestId, "user" to (user?.name ?: "not found"))
+    }
+
     runApplication<SpringBootApp>(*args)
 }
