159f8a02684ca1b885d657d2aa2ad3c6063e58ac
mvukic <matija.vukic2@gmail.com>
2023-10-22 15:07:08 +0200

    u

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "3.2.0-SNAPSHOT"
+    id("org.springframework.boot") version "3.2.0-RC1"
     id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.9.20-Beta2"
-    kotlin("plugin.spring") version "1.9.20-Beta2"
-    kotlin("plugin.serialization") version "1.9.20-Beta2"
+    kotlin("jvm") version "1.9.20-RC"
+    kotlin("plugin.spring") version "1.9.20-RC"
+    kotlin("plugin.serialization") version "1.9.20-RC"
 }
 
 group = "org.mvukic"
@@ -18,7 +18,6 @@ kotlin {
 repositories {
     mavenCentral()
     maven { url = uri("https://repo.spring.io/milestone") }
-    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 configurations {
@@ -47,7 +46,7 @@ tasks {
     }
 
     wrapper {
-        version = "8.4-rc-1"
+        version = "8.4"
     }
 
     compileKotlin {
