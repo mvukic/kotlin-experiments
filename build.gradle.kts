7d03ced361c51ff41a056745574b2038db35aca1
mvukic <matija.vukic2@gmail.com>
2024-07-20 10:06:07 +0200

    u

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.5"
+    id("org.springframework.boot") version "3.4.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.1.6"
     id("org.graalvm.buildtools.native") version "0.10.2"
-    kotlin("jvm") version "2.0.20-Beta1"
-    kotlin("plugin.spring") version "2.0.20-Beta1"
-    kotlin("plugin.serialization") version "2.0.20-Beta1"
+    kotlin("jvm") version "2.0.20-Beta2"
+    kotlin("plugin.spring") version "2.0.20-Beta2"
+    kotlin("plugin.serialization") version "2.0.20-Beta2"
 }
 
 group = "org.mvukic"
@@ -32,13 +32,13 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation(kotlinx("datetime:0.6.0"))
-    implementation(kotlinx("serialization-json:1.7.0"))
+    implementation(kotlinx("serialization-json:1.7.1"))
     implementation(kotlinx("coroutines-core:1.9.0-RC"))
     implementation(kotlinx("coroutines-reactor:1.9.0-RC"))
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 
     // Logging
-    implementation("io.klogging:klogging-spring-boot-starter:0.5.14")
+    implementation("io.klogging:klogging-spring-boot-starter:0.6.1")
 
     // Spring Boot Starter
     implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -56,7 +56,7 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:neo4j")
-    testImplementation("org.neo4j.driver:neo4j-java-driver:5.21.0")
+    testImplementation("org.neo4j.driver:neo4j-java-driver:5.22.0")
     implementation(kotlinx("coroutines-test:1.9.0-RC"))
 }
 
@@ -66,7 +66,7 @@ tasks {
     }
 
     wrapper {
-        version = "8.8"
+        version = "8.9"
     }
 
     compileKotlin {
